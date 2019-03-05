38ea8dc3c7e84ab2dfae0da232df20471af3eebc
FZJ <yudwcom@163.com>
2019-03-05 14:39:49 +0800

    用户

---
+++ application/mobile/controller/Member.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 用户 的 PHP 后端
+ */
+namespace app\mobile\controller;
+
+use think\Db;
+use app\common\model\WxNews;
+ 
+class Member extends MobileBase
+{
+     
+    /**
+     * 用户首页
+     */
+    public function index()
+    {
+       
+        return $this->fetch();
+    }
+
+
+
+    /**
+     * 个人资料
+     */
+    public function userinfo()
+    {
+       
+        return $this->fetch();
+    }
+    
+  
+    
+}

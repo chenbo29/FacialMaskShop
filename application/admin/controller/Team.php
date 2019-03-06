dd6b0da211f4a79761757b36f7ac0acc6b9a5154
lx <13265947403@163.com>
2019-03-06 12:17:21 +0800

    admin模块

--- application/admin/controller/Team.php
+++ application/admin/controller/Team.php
@@ -28,99 +28,12 @@ use think\Page;
 
 class Team extends Base
 {
-	public function index()
-	{
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
+	public function index(){
+		
+		return $this->fetch();
 	}
-
-	/**
-	 * 拼团详情
-	 * @return mixed
-	 */
-	public function info()
-	{
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 保存
-	 * @throws \think\Exception
-	 */
-	public function save(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 删除拼团
-	 */
-	public function delete(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");
-	}
-
-	/**
-	 * 确认拼团
-	 * @throws \think\Exception
-	 */
-	public function confirmFound(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 拼团退款
-	 */
-	public function refundFound(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 拼团抽奖
-	 */
-	public function lottery(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 拼团订单
-	 */
-	public function team_list()
-	{
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 拼团订单详情
-	 * @return mixed
-	 */
-	public function team_info()
-	{
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	//拼团订单
-	public function order_list(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	/**
-	 * 团长佣金
-	 */
-	public function bonus(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
-	}
-
-	public function doBonus(){
-	header("Content-type: text/html; charset=utf-8");
-exit("请联系TPshop官网客服购买高级版支持此功能");	
+	public function info(){
+		
+		return $this->fetch();
 	}
 }

2df0eacffbd3772ac601aec2a5f86ffab9f670ee
FZJ <yudwcom@163.com>
2019-03-06 19:28:22 +0800

    签到、业绩

---
+++ application/mobile/controller/Sign.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 签到
+ */
+namespace app\mobile\controller;
+
+use think\Db;
+use think\Page;
+use app\common\logic\ActivityLogic;
+
+class Sign extends MobileBase {
+
+
+    public function index(){     
+        
+        
+
+        return $this->fetch();
+    }
+
+
+  
+
+}

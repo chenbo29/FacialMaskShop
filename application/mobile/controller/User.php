040d55bc86c444491c66e7c6c7826fe56fd65526
fuzhijingcom <yudwcom@163.com>
2019-03-07 11:23:02 +0800

    用户

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * 智丰网络
- * ============================================================================
- * * 版权所有 2015-2027 深圳搜豹网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.tp-shop.cn
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和使用 .
- * 不允许对程序代码以任何形式任何目的的再发布。
- * 采用最新Thinkphp5助手函数特性实现单字母函数M D U等简写方式
- * ============================================================================
- * 2015-11-21
- */
+
 namespace app\mobile\controller;
 
 use app\common\logic\CartLogic;
@@ -78,9 +67,13 @@ class User extends MobileBase
 
     public function index()
     {
+
         $MenuCfg = new MenuCfg();
         $menu_list = $MenuCfg->where('is_show', 1)->order('menu_id asc')->select();
         $this->assign('menu_list', $menu_list);
+
+        
+
         return $this->fetch();
     }
 

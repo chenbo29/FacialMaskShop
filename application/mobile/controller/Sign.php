d8df6ef8b274c0ff30a8cea4a150a725810082c1
hsj628 <13690837@163.com>
2019-03-09 17:05:41 +0800

    sign签到

--- application/mobile/controller/Sign.php
+++ application/mobile/controller/Sign.php
@@ -63,6 +63,8 @@ class Sign extends MobileBase {
 
     /**
      * 签到
+     *
+     * 点击签到
      */
     public function sign(){
         $user_id = I('user_id');
@@ -73,13 +75,17 @@ class Sign extends MobileBase {
         $con['sign_day'] = array('like',date('Y-m-d',time()).'%');
         $cunzai = M('sign_log')->where(['user_id'=>$user_id])->where($con)->find();
 
+        $date= $this->deal_time(date('Y-m-d H:i:s'));
+
         if($cunzai){
-            return $this->ajaxReturn(['status'=>1,'msg'=>'今日已签到']);
+            return $this->ajaxReturn(['status'=>1,'msg'=>'今日已签到','date'=>$date]);
         }else{
             $r = M('sign_log')->save(['user_id'=>$user_id,'sign_day'=>date('Y-m-d H:i:s')]);
             if($r){
                 if($r){
-                    return $this->ajaxReturn(['status'=>1,'msg'=>'签到成功']);
+//                    $data=get_again_sign_day($user_id);
+
+                    return $this->ajaxReturn(['status'=>1,'msg'=>'签到成功','date'=>$date]);
                 }else{
                     return $this->ajaxReturn(['status'=>-1,'msg'=>'签到失败']);
                 }
@@ -89,6 +95,10 @@ class Sign extends MobileBase {
 
 
     /**
+     *
+     * 页面加载
+     *
+     *
      * 获取签到的日期列表
      */
     public function get_sign_day(){
@@ -116,10 +126,13 @@ class Sign extends MobileBase {
 
         $add_point = 10;
 
+        //连续签到几天
         $continue_sign = $this->continue_sign($user_id);
 
-        //连续签到几天
+        //累计签到几天
         $accumulate_day = count($data);
+
+        $rule = '规则';
 
         return $this->ajaxReturn(
             ['status'=>1,
@@ -129,7 +142,57 @@ class Sign extends MobileBase {
             'points'=>$points,
             'add_point'=>$add_point,
             'continue_sign'=> $continue_sign,
-            'accumulate_day'=>$accumulate_day]);
+            'accumulate_day'=>$accumulate_day,
+                'note'=>$rule
+
+            ]);
+    }
+    //在签到的时候通过传值的方式再次调用
+    public function get_again_sign_day($user_id){
+//        $user_id = I('user_id');
+        if(!$user_id){
+            return $this->ajaxReturn(['status'=>-1,'msg'=>'user_id不能为空','data'=>'']);
+        }
+        $list = M('sign_log')->where(['user_id'=>$user_id])->field('sign_day')->select();
+        foreach($list as $k => $v){
+            $data[$k] = $this->deal_time($v['sign_day']);
+        }
+
+
+        $con['sign_day'] = array('like',date('Y-m-d',time()).'%');
+        $cunzai = M('sign_log')->where(['user_id'=>$user_id])->where($con)->find();
+
+        if($cunzai){
+            $today_sign = true;
+        }else{
+            $today_sign = false;
+        }
+
+        //当前积分
+        $points = M('users')->where(['user_id'=>$user_id])->value('pay_points');
+
+        $add_point = 10;
+
+        //连续签到几天
+        $continue_sign = $this->continue_sign($user_id);
+
+        //累计签到几天
+        $accumulate_day = count($data);
+
+        $rule = '这里是规则这里是规则这里是规则这里是规则这里是规则这里是规则这里是规则这里是规则';
+
+        return $this->ajaxReturn(
+            ['status'=>1,
+            'msg'=>'获取成功',
+            'data'=>$data,
+            'today_sign'=>$today_sign,
+            'points'=>$points,
+            'add_point'=>$add_point,
+            'continue_sign'=> $continue_sign,
+            'accumulate_day'=>$accumulate_day,
+                'note'=>$rule
+
+            ]);
     }
 
     /**
@@ -146,21 +209,58 @@ class Sign extends MobileBase {
      * 连续签到次数
      */
     private function continue_sign($user_id){
+        //定义时间戳
+        date_default_timezone_set("Asia/Shanghai");
 
-        $first_sign_day = M('sign_log')->where(['user_id'=>$user_id])->order('id desc')->value('sign_day');
-        //判断一下这个 日期连续签到次数
-        if(!$first_sign_day){
-            return 0;
+        //先看一下今天有没有签到
+        $con['sign_day'] = array('like',date('Y-m-d',time()).'%');
+        $cunzai = M('sign_log')->where(['user_id'=>$user_id])->where($con)->find();
+        if($cunzai){
+            $todaySign=1;
+        }else{
+            $todaySign=0;
+        }
+        //再看之前的签到时间
+        $list = M('sign_log')->where(['user_id'=>$user_id])->order('sign_day desc')->field('sign_day')->select();
+        //对所有的签到时间进行时间戳然后倒序排序
+        $array=array();
+        foreach($list as $key=>$value){
+            $array[]=strtotime($value['sign_day']);
         }
 
-        $continue_sign = 1;
+        //定义连续签到次数
+        $countSign=$todaySign;
+        //依次判断所有的时间戳是否在指定范围内，例如第一个应该在昨天00:00:00-23:59:59之前，如果在则$countSign+1,否则跳出循环
+        //定义昨天的时间戳范围
+        $begintime=strtotime(date('Y-m-d 00:00:00',time()-86400));
+        $endtime=strtotime(date('Y-m-d 23:59:59',time()-86400));
+        if($todaySign==1){
+            for($i=1;$i<count($array);){
+//                echo $begintime."------".$array[$i]."---------".$endtime."+++++";
+                if($array[$i]>=$begintime && $array[$i]<=$endtime){
+                    $countSign++;
+                    $begintime-=86400;
+                    $endtime-=86400;
+                }else{
+                    break;
+                }
+                $i++;
+            }
+        }else{
+            for($k=0;$k<count($array);){
+                if($array[$k]>=$begintime && $array[$k]<=$endtime){
+                    $countSign++;
+                    $begintime-=86400;
+                    $endtime-=86400;
+                }else{
+                    break;
+                }
+                $k++;
+            }
+        }
 
-        $sign_day_2 = date('Y-m-d',strtotime("$first_sign_day -1 day")) ;
-        
-
-
-
-
-        return $continue_sign;
+//        echo $begintime."``````````````".$endtime;
+//        var_dump($array);die;
+        return $countSign;
     }
 }

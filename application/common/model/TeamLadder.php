78dd927d4b7aa4fe6ead675d2be7dc222cd0d7ca
zhifeng01 <593987816@qq.com>
2019-03-09 10:00:48 +0800

    拼团页面修改

---
+++ application/common/model/TeamLadder.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 智丰网络
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/8
+ * Time: 18:42
+ */
+
+namespace app\common\model;
+use think\Model;
+use think\Db;
+
+class TeamLadder extends Model
+{
+
+}
